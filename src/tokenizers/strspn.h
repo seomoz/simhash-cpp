cd43e62c2179f5e36b7035fc8edc18e3d3c12316
Brandon Forehand <brandon@moz.com>
2015-02-03 10:45:37 -0800

    Fix missing include header for `::strspn`.

--- src/tokenizers/strspn.h
+++ src/tokenizers/strspn.h
@@ -1,6 +1,8 @@
 #ifndef SIMHASH_TOKENIZERS__UTIL_H
 #define SIMHASH_TOKENIZERS__UTIL_H
 
+#include <string.h>
+
 namespace Simhash {
     struct Strspn {
         /* Return the length of the token starting at last */
