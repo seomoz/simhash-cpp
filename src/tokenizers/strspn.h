127c95c39c8c35accf930096fd86dbd2cde30481
Dan Lecocq <dan@moz.com>
2015-02-03 13:05:54 -0800

    Added a hash(const char*, Tokenizer) interface

--- src/tokenizers/strspn.h
+++ src/tokenizers/strspn.h
@@ -6,7 +6,7 @@
 namespace Simhash {
     struct Strspn {
         /* Return the length of the token starting at last */
-        const char* operator()(const char* last) {
+        const char* operator()(const char* last) const {
             size_t s = strspn(last,
                 "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
             return (*last != '\0') ? last + s : NULL;
