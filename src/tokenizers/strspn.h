e6f29d25888a3a3ffe9184c409c50e189f0cb427
vadim suvorov <vadim@moz.com>
2016-05-19 11:26:18 -0700

    Refactor tokenizer to avoid off by one error

--- src/tokenizers/strspn.h
+++ src/tokenizers/strspn.h
@@ -4,17 +4,28 @@
 #include <string.h>
 
 namespace Simhash {
+    static const char* alphas = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    struct StringToken {
+        const char* start;
+        size_t      length;
+    };
+
     struct Strspn {
         /**
-         * Return a pointer to the end of the next token or NULL if
-         * the end of string has been reached.
+         * Sets fields of the `output` to the first character of the
+         * token and its length, skipping preceding non-token characters.
+         * If the `string` does not contain any token (empty or constists
+         * of separators only), `output.start` is set to NULL, and
+         * `output.length` is set to 0.
+         *
+         * Note that for valid token length is never 0.
          */
-        const char* operator()(const char* last) const {
-            size_t s = strspn(last,
-                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-            return (*last != '\0') ? last + s : NULL;
+        void operator()(const char* string, StringToken& output) const {
+            const char* start = strpbrk(string, alphas);
+            output.start = start;
+            output.length = (start ? strspn(start, alphas) : 0);
         }
     };
 }
-
 #endif
--- src/tokenizers_test.cpp
+++ src/tokenizers_test.cpp
@@ -1,43 +1,73 @@
 #include "catch.hpp"
 
+#include <string.h>
+
 /* Include internals */
 #include "tokenizers/strspn.h"
 
+void test_tokenizer(const char* string, const char* expected_tokens[]) {
+    Simhash::Strspn tokenizer;
+    Simhash::StringToken token;
+
+    const char** expected = expected_tokens;
+    for (tokenizer(string, token)
+        ; token.length != 0
+        ; tokenizer(token.start + token.length, token)) {
+        REQUIRE(((*expected) != NULL));
+        REQUIRE((token.start != NULL));
+        REQUIRE((token.length == strlen(*expected)));
+        REQUIRE((strncmp(token.start, *expected, token.length)== 0));
+        ++expected;
+    }
+    REQUIRE((token.start == NULL));
+    REQUIRE((token.length == 0));
+}
+
 TEST_CASE("We can get tokens from a string", "[tokenizers]") {
-    /* This is a string that we'd like to use for testing the tokenizer */
-    const char* test_string = "what's new?How5is _ the ^# stuff";
+    SECTION("We can tokenize an empty string") {
+        const char* string = "";
+        const char* expected_tokens[] = {NULL};
 
-    SECTION("We can use the strspn tokenizer") {
-        /* We need an instance of our actual tokenizer */
-        Simhash::Strspn tokenizer;
+        test_tokenizer(string, expected_tokens);
+    }
 
-        const char *current(NULL);
-        const char *next(NULL);
-        current = test_string;
-
-        int64_t expected[] = {
-            4, // 'what'
-            1, // 's'
-            3, // 'new'
-            3, // 'How'
-            2, // 'is'
-            0, // '_'
-            0, // ' '
-            3, // 'the'
-            0, // '^'
-            0, // '#'
-            0, // ' '
-            5, // 'stuff'
+    SECTION("We can tokenize long string") {
+        /* This is a string that we'd like to use for testing the tokenizer */
+        const char* string = "what's new?How5is _ the ^# stuff";
+        const char* expected_tokens[] = {
+            "what", "s", "new", "How", "is", "the", "stuff", NULL
         };
 
-        for (size_t i = 0; i < 12; ++i) {
-            next = tokenizer(current);
-            REQUIRE((next - current) == expected[i]);
-            current = next + (*next != '\0');
-        }
+        test_tokenizer(string, expected_tokens);
+    }
 
-        /* Now we should be at the end */
-        next = tokenizer(current);
-        REQUIRE((next == NULL));
+    SECTION("We can tokenize string that starts with delimiters") {
+        /* This is a string that we'd like to use for testing the tokenizer */
+        const char* string = "@#$@$! what's new?How5is _ the ^# stuff";
+        const char* expected_tokens[] = {
+            "what", "s", "new", "How", "is", "the", "stuff", NULL
+        };
+
+        test_tokenizer(string, expected_tokens);
+    }
+
+    SECTION("We can tokenize string that ends with delimiters") {
+        /* This is a string that we'd like to use for testing the tokenizer */
+        const char* string = "what's new?How5is _ the ^# stuff !~@#!@$";
+        const char* expected_tokens[] = {
+            "what", "s", "new", "How", "is", "the", "stuff", NULL
+        };
+
+        test_tokenizer(string, expected_tokens);
+    }
+
+    SECTION("We do not overstep end-of-string") {
+        /* This is a string that we'd like to use for testing the tokenizer */
+        const char* string = "a b c\0d e f\0\0";
+        const char* expected_tokens[] = {
+            "a", "b", "c", NULL
+        };
+
+        test_tokenizer(string, expected_tokens);
     }
 }
