904213050b42fab2e279fbb2240ffbff5bcc48f2
vadim suvorov <vadim@moz.com>
2016-05-31 12:05:18 -0700

    refactor simhash for Python and simplicity

--- src/hash.hpp
+++ src/hash.hpp
@@ -1,6 +1,8 @@
 #ifndef SIMHASH_HASH_H
 #define SIMHASH_HASH_H
 
+#include <vector>
+
 /* Simply holds references to all of our hash functions */
 #include "hashes/jenkins.h"
 #include "hashes/murmur.h"
@@ -13,115 +15,138 @@
 #include "common.h"
 #include "cyclic.hpp"
 
-#include <vector>
-
 namespace Simhash {
-    /**
-     * Compute a similarity hash for the provided string using a rolling hash
-     * function and a provided hash window
-     *
-     * @param tokens - An NULL-terminated array of character pointers to the
-     *                 nul-terminated tokens comprising the text.
-     *
-     * @return hash representative of the content of the text */
-    template <typename Hash=jenkins>
-    class Simhash {
-    private:
-        class Accumulator {
-        private:
-            static const size_t BITS = sizeof(hash_t) * 8;
-
-            size_t window;
-            Cyclic<hash_t> cyclic;
-            std::vector<size_t> v;
-            size_t hash_count;
-        public:
-            Accumulator() : window(3), cyclic(window), v(BITS, 0), hash_count(0) {}
-
-            void update(hash_t hash) {
-                hash = cyclic.push(hash);
-                ++hash_count;
-                for (int j = BITS - 1; j >= 0; --j) {
-                    v[j] += hash & 1;
-                    hash >>= 1;
-                }
-            }
-
-            /* With counts appropriately tallied, create a 1 bit for each of
-             * the counts that's over threshold. That result is the hash. */
-            hash_t result() {
-                size_t threshold = hash_count / 2;
-                hash_t hash(0);
-                for (size_t j = 0; j < BITS; ++j) {
-                    if (v[j] > threshold) {
-                        hash |= (static_cast<hash_t>(1) << j);
-                    }
-                }
-                return hash;
-            }
-        };
+    class HasherInterface {
     public:
-        typedef Hash      hash_type;
+        virtual void update(hash_t) = 0;
+        virtual hash_t result() = 0;
+    };
 
-        /* Alias of operator()
-         *
-         * Some languages have difficulty making use of operator(), and so this
-         * it made available for those languages */
-        inline hash_t hash(char **tokens) {
-            return operator()(tokens);
+    class Simhash: public HasherInterface {
+    private:
+        static const size_t BITS = sizeof(hash_t) * 8;
+
+        std::vector<size_t> bit_counts;
+        size_t hash_count;
+    public:
+        Simhash(): bit_counts(BITS, 0), hash_count(0) {}
+
+        /**
+         * Updates bit counts with pre-calculated hash.
+         */
+        virtual void update(hash_t hash) /* override */ {
+            ++hash_count;
+            for (int j = BITS - 1; j >= 0; --j) {
+                bit_counts[j] += hash & 1;
+                hash >>= 1;
+            }
         }
 
-        /* Return a simhash value using a moving window */
-        hash_t operator()(char **tokens) {
-            Hash hasher;
-            Accumulator accumulator;
-
-            for (char **tp = tokens; *tp != NULL; ++tp) {
-                size_t len = strlen(*tp);
-                if (len > 0) {
-                    accumulator.update(hasher(*tp, len, 0));
+        /* With bit counts appropriately tallied, create a 1 bit for each of
+         * the counts that's over threshold. That result is the hash. */
+        virtual hash_t result() /* override */ {
+            size_t threshold = hash_count / 2;
+            hash_t hash(0);
+            for (size_t j = 0; j < BITS; ++j) {
+                if (bit_counts[j] > threshold) {
+                    hash |= (static_cast<hash_t>(1) << j);
                 }
             }
-
-            return accumulator.result();
-        }
-
-        /* As above, but operate on a vector of unsigned 64-bit numbers,
-           not strings. */
-        hash_t hash_fp(uint64_t *vec, int len) {
-            Hash hasher;
-            Accumulator accumulator;
-
-            for (int i = 0; i < len; ++i) {
-                accumulator.update(
-                    hasher(reinterpret_cast<char*>(vec+i), sizeof(uint64_t), 0));
-            }
-
-            return accumulator.result();
-        }
-
-        /* As above, but operate on a vector of signed 64-bit integers. For
-         * some language bindings, casting and function overloading can be
-         * difficult to use, which is why it's given a new name */
-        hash_t hash_fps(int64_t *vec, int len) {
-            return hash_fp(reinterpret_cast<uint64_t*>(vec), len);
-        }
-
-        template <typename Tokenizer>
-        hash_t hash_tokenizer(const char* string, const Tokenizer& tokenizer) {
-            Hash hasher;
-            Accumulator accumulator;
-            StringToken token;
-
-            for (tokenizer(string, token)
-                ; token.length != 0
-                ; tokenizer(token.start + token.length, token)) {
-                accumulator.update(hasher(token.start, token.length, 0));
-            }
-
-            return accumulator.result();
+            return hash;
         }
     };
-}
 
+    class MovingWindowShingler: public HasherInterface {
+    private:
+        HasherInterface& hasher;
+        int window_size;
+        Cyclic<hash_t> cyclic;
+
+    public:
+        MovingWindowShingler(HasherInterface& hasher_, int window_size_)
+            : hasher(hasher_), window_size(window_size_), cyclic(window_size_) {
+        }
+
+        virtual void update(hash_t hash) /* override */ {
+            hasher.update(cyclic.push(hash));
+        }
+
+        /* With bit counts appropriately tallied, create a 1 bit for each of
+         * the counts that's over threshold. That result is the hash. */
+        virtual hash_t result() /* override */ {
+            for (int j = 0; j < window_size - 1; ++j) {
+                hasher.update(cyclic.push(0));
+            }
+            return hasher.result();
+        }
+    };
+
+    class MovingWindowLegacyShingler: public HasherInterface {
+    private:
+        HasherInterface& hasher;
+        Cyclic<hash_t> cyclic;
+
+    public:
+        MovingWindowLegacyShingler(HasherInterface& hasher_, int window_size_)
+            : hasher(hasher_), cyclic(window_size_) {
+        }
+
+        virtual void update(hash_t hash) /* override */ {
+            hasher.update(cyclic.push(hash));
+        }
+
+        /* With bit counts appropriately tallied, create a 1 bit for each of
+         * the counts that's over threshold. That result is the hash. */
+        virtual hash_t result() /* override */ {
+            return hasher.result();
+        }
+    };
+
+    template <typename HashFunctor>
+    class DataHasher {
+    private:
+        HasherInterface& hasher;
+        HashFunctor data_hash;
+    public:
+        DataHasher(HasherInterface& hasher_)
+            : hasher(hasher_) {
+        }
+
+        void update(const void* data, size_t data_size) {
+            hash_t hash =
+                data_hash(static_cast<const char*>(data), data_size, 0);
+            hasher.update(hash);
+        }
+
+        hash_t result() {
+            return hasher.result();
+        }
+    };
+
+    typedef DataHasher<fnv> FnvDataHasher;
+    typedef DataHasher<jenkins> JenkinsDataHasher;
+    typedef DataHasher<murmur> MurmurDataHasher;
+
+
+    /**
+     * Calculates simhash of the `text` using provided
+     * SimHash and Tokenizer classes.
+     */
+    template<typename HashFunctor, typename Tokenizer>
+    hash_t hash_text(const char* string) {
+        Simhash simhash;
+        MovingWindowLegacyShingler shingler(simhash, 3);
+        DataHasher<HashFunctor> hasher(shingler);
+        Tokenizer tokenizer;
+        StringToken token;
+
+        for (tokenizer(string, token)
+            ; token.length != 0
+            ; tokenizer(token.start + token.length, token)) {
+            hasher.update(token.start, token.length);
+        }
+
+        return hasher.result();
+    }
+}
 #endif
