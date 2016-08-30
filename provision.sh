dffdeec7598736d0a002c11444914350a179d992
Dan Lecocq <dan@moz.com>
2016-08-30 15:10:01 -0700

    Travis.

--- provision.sh
+++ provision.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get install -y make g++ gdb git cmake libgtest-dev
 
 pushd /usr/src/gtest
-    sudo cmake CMakeLists.txt
-    sudo make
+    sudo CXX=g++ CC=gcc cmake CMakeLists.txt
+    sudo CXX=g++ CC=gcc make
     find /usr/src/gtest -name '*.a' | xargs -I^ sudo ln -sf ^ /usr/lib
 popd
